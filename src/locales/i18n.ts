35137ed4058ce3328bbfea215ad2350ae0d27f61
Rafał Ileczko <rileczko@gmail.com>
2022-03-23 22:38:07 +0100

    Add buttons titles

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -9,9 +9,6 @@ i18n
   .use(LanguageDetector)
   .init({
     resources: { pl, en },
-    lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
     debug: true,
     fallbackLng: "pl",
     defaultNS: "common",

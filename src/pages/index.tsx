014b639d5a5ecbf110f90a3851e6dfd894868a9e
Rafał Ileczko <rileczko@gmail.com>
2023-05-21 13:12:08 +0200

    Add twitter meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,28 +2,16 @@ import { Sidebar } from "@/components/Sidebar";
 import { Layout } from "@/components/Layout";
 import { Header } from "@/components/Header";
 import { ContentSection } from "@/components/ContentSection";
-import { NextPageContext } from "next";
 import { useTranslation } from "react-i18next";
 import Head from "next/head";
 
-export const getServerSideProps = (context: NextPageContext) => {
-  const headers = context.req?.headers || {};
-  console.log("origin", headers);
-
-  const protocol =
-    headers["x-forwarded-proto"] || headers.host?.startsWith("localhost")
-      ? "http"
-      : "https";
-
-  return { props: { origin: `${protocol}//${headers.host}` } };
-};
-
-const IndexPage = ({ origin }: { origin: string }) => {
+const IndexPage = () => {
   const { t } = useTranslation();
 
   const title = `${t("title") || "Rafał Ileczko"} - ${
     t("subtitle") || "Senior Developer"
   }`;
+
   return (
     <>
       <Head>
@@ -32,7 +20,15 @@ const IndexPage = ({ origin }: { origin: string }) => {
         <meta property="og:title" content="Rafał Ileczko" />
         <meta property="og:description" content="Senior Software Developer" />
         <meta property="og:image" content={`https://rileczko.com/api/og`} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="rileczko.com" />
+        <meta property="twitter:url" content="https://rileczko.com" />
+        <meta name="twitter:title" content="Rafał Ileczko" />
+        <meta name="twitter:description" content="Senior Software Developer" />
+        <meta name="twitter:image" content="https://rileczko.com/api/og" />
       </Head>
+
       <Layout sidebar={<Sidebar />} header={<Header />}>
         <div className="flex flex-col gap-8">
           <ContentSection i18nKey="experience" />
